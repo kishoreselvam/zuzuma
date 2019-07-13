33687473f61859df3738954908dc56e561340779
kishoreselvam <51222623+kishoreselvam@users.noreply.github.com>
2019-07-13 14:53:10 +0530

    Update a3.py

--- a3.py
+++ a3.py
@@ -1,8 +1,8 @@
-num = int(input("Enter any number: "))
-flag = num%2
+numb = int(input("Enter any number: "))
+flag = numb%2
 if flag == 0:
-    print(num,"is an even number")
+    print(numb,"is an even number")
 elif flag == 1:
-    print(num,"is an odd number")
+    print(numb,"is an odd number")
 else:
     print("Error, Invalid input")
