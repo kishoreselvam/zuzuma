cff9396a7e3d04e048a5eb56f613d847713eded5
kishoreselvam <51222623+kishoreselvam@users.noreply.github.com>
2019-07-14 21:32:39 +0530

    Create ee1.py

---
+++ ee1.py
@@ -0,0 +1,12 @@
+cd=int(input())
+ji=[]
+k = list(map(int,input().split()))
+c1=[]
+for y in k:
+    if(k.count(y)>k):
+        cl.append(y)
+if (len(cl)>=2):
+    d=set(cl)
+    print(*d)
+else:
+    print('unique')
